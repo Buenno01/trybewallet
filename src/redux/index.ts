110ba37fdfcfd4070c2ca25717a7f6f47c2dd78a
Vinicius Bueno Costa <buenov535@gmail.com>
2024-01-29 15:32:10 -0300

    first commit

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,1 +1,9 @@
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { combineReducers, legacy_createStore as createStore } from 'redux';
+
+const rootReducer = combineReducers({});
+
 // configure aqui sua store
+const store = createStore(rootReducer, composeWithDevTools());
+
+export default store;
