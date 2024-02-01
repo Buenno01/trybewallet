3a465408e4dcc7b26e40c77c1d9ce0a78d265d40
Vinicius Bueno Costa <buenov535@gmail.com>
2024-02-01 13:51:47 -0300

    refactor: o tipo do estado global é inferido pelo tipo do rootReducer

--- src/@types/GlobalStateType.ts
+++ src/@types/GlobalStateType.ts
@@ -1,7 +1,7 @@
-import { UserType } from './UserType';
-import { WalletType } from './WalletType';
+import { ThunkDispatch } from 'redux-thunk';
+import { AnyAction } from 'redux';
+import rootReducer from '../redux/reducers';
 
-export type GlobalStateType = {
-  user: UserType,
-  wallet: WalletType,
-};
+export type GlobalStateType = ReturnType<typeof rootReducer>;
+
+export type GlobalDispatch = ThunkDispatch<GlobalStateType, null, AnyAction>;
